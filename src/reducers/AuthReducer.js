74d5aca8b3b7be7401be66bfb3d8b6681c64b327
Mayur Raval <mayurraval@asite.com>
2019-01-18 10:09:23 +0530

    second commit

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,10 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
+const INITIAL_STATTE = { email: ''};
+
+export default (state =INITIAL_STATTE ,action) => {
+  switch(action.type){
+      default:
+        return state;
+  }
+};
